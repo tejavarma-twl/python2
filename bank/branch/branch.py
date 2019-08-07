1b9e53e8df3c3215e9e166be9f737d0fb7f0965b
Bob <avinash.a.6577@gmail.com>
2019-08-07 11:52:06 +0530

    xampp setup. Db and table creation in mysql using localhost/phpmyadmin

--- bank/branch/branch.py
+++ bank/branch/branch.py
@@ -5,6 +5,12 @@
 # bank_branchcode
 # bank_address
 
+# CREATE TABLE `bank`.`branch` ( `bank_id` INT NOT NULL AUTO_INCREMENT , 
+# `bank_name` VARCHAR(30) NOT NULL , `bank_branch` VARCHAR(20) NOT NULL , 
+# `bank_ifsc` VARCHAR(15) NOT NULL , `bank_branchcode` VARCHAR(10) NOT NULL , 
+# `bank_address` TEXT NOT NULL , PRIMARY KEY (`bank_id`)) ENGINE = InnoDB;
+
+
 def creation():
     pass
 
--- bank/transactions/transaction.py
+++ bank/transactions/transaction.py
@@ -1,10 +1,10 @@
-# trans_id
-# trans_sender
-# trans_receiver
-# trans_amount
-# trans_mode
-# trans_date
-# trans_time
+# trans_id trans_sender trans_receiver trans_amount trans_mode trans_date trans_time
+
+# CREATE TABLE `bank`.`transactions` ( `trans_id` INT NOT NULL AUTO_INCREMENT , 
+# `trans_sender` INT NOT NULL , `trans_receiver` INT NOT NULL , 
+# `trans_amount` FLOAT NOT NULL , `trans_mode` VARCHAR(5) NOT NULL , 
+# `trans_createdon` DATETIME NOT NULL , `trans_status` VARCHAR(10) NOT NULL , 
+# PRIMARY KEY (`trans_id`)) ENGINE = InnoDB;
 
 transactions = [
     {'trans_id':'123','trans_sender':'teja','trans_receiver':'avi',
@@ -16,24 +16,36 @@ transactions = [
 {'trans_id':'123','trans_sender':'hari','trans_receiver':'teja',
 'trans_amount':'2200000','trans_mode':'rtgs','trans_date':'6/19','trans_time':'9:00 am'},
 ]
+
 mytransaction = []
 mytransaction_debit = []
-mytransaction_credit = []
+mytransaction_credit = []am
+
+
 def transfer():  #Transaction creation
     pass
 
 def debited():    #Debit details of account
     for trans in transactions:
-        if trans['trans_sender']==='teja':
+        if trans['trans_sender']=='teja':
             mytransaction_debit.append(trans)
+    print(mytransaction_debit)
 
 def credited():   #Credit details
     for trans in transactions:
-        if trans['trans_receiver']==='teja':
+        if trans['trans_receiver']=='teja':
             mytransaction_credit.append(trans)
+    print(mytransaction_credit)
 
 def allTransactions():   #Credit details
     for trans in transactions:
-        if trans['trans_sender']==='teja' or trans['trans_receiver']==='teja':
+        if trans['trans_sender']=='teja' or trans['trans_receiver']=='teja':
             mytransaction.append(trans)
+    print(mytransaction)
 
+print("Debit Transactions")
+debited()
+print("Credit Transactions")
+credited()
+print("All Transactions")
+allTransactions()
