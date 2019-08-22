7de26f0ba3197bc91dc65bd31bc3037584152bcb
Bob <avinash.a.6577@gmail.com>
2019-08-22 11:59:26 +0530

    class modules

---
+++ class/calc.py
@@ -0,0 +1,16 @@
+class Calculate:
+  def addition(a,b):
+    return a+b
+
+  def subtraction(a,b):
+    return a-b
+
+  def multiply(a,b):
+    return a*b
+
+  def divide(a,b):
+    return a/b
+
+a = 10
+def showShomMessage():
+  print("Showing!")
--- class/main.py
+++ class/main.py
@@ -1,7 +1,11 @@
+import calc
 class Main:
-    def __init__(self):
+    def __init__(self,a,b):
         self.common = ["Hello"]
         self.greet = "Hey guys!"
+        self.a = a
+        self.b = b
+        # self.res = 0
 
     def display(self):
         print(self.greet)
@@ -9,11 +13,24 @@ class Main:
     def changeGreeting(self):
         self.greet = "Hello guys!"
 
-main = Main()
-main2 = Main()
-main.common.append("Bolo")
-print(main.common)
-print(main2.common)
+    def calculation(self,a,b):
+        cal = calc.Calculate
+        self.res = cal.addition(a,b)
+        # print(res)
+
+    def getResult(self):
+        return self.res
+
+main = Main(1,2)
+main2 = Main(1,2)
+Main.calculation(main,3,4)
+Main.calculation(main2,5,4)
+# print(main.__dict__)
+# print(main2.__dict__)
+# main2 = Main()
+# main.common.append("Bolo")
+# print(main.common)
+# print(main2.common)
 
 # main.common = "Hi"
 # print(main.common)
